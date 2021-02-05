ccb5a22b0dcf8391dabfb1eb17a8d41233f0ecae
Sergio Gómez <sgomez@vise.com.mx>
2021-02-05 00:22:27 -0600

    cambio en el modulo

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -50,7 +50,7 @@ import { ContactComponent } from './contact/contact.component';
     RegisterpageComponent,
     LandingpageComponent,
     AboutmeComponent,
-    ProjectsComponent,
+    //ProjectsComponent,
     ContactComponent
   ],
   exports: [
